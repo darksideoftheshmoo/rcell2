1cb71290ef4176e725c88e6e934d157996bcedd6
german <gerbeldo@gmail.com>
2019-03-18 22:11:31 -0300

    adds several namespace imports for cimage

--- R/imports.R
+++ R/imports.R
@@ -1,4 +1,5 @@
-#' @importFrom graphics image
+#' @importFrom graphics image lines par plot.new text
 #' @importFrom stats na.omit
-#' @importFrom utils glob2rx sessionInfo globalVariables
+#' @importFrom utils glob2rx sessionInfo globalVariables capture.output head read.delim write.table
+#' @importFrom grDevices col2rgb dev.list dev.off dev.size tiff
 NULL
