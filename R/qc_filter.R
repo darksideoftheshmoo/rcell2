2734550b33f798112b5395a28839bc5f51e63069
german <german@MacBook.local>
2020-07-17 13:17:08 -0300

    add qc_filter documentation

--- R/qc_filter.R
+++ R/qc_filter.R
@@ -1,11 +1,19 @@
 #public
-#filters cells, modifies QC variable
-#ToDo: warn for posible outdated variables
+
 #' qc_filter
 #'
-#' @param X
-#' @param filter
-#' @param subset
+#' Filters cells that DO NOT MATCH the condition in \code{filter}. i.e. if you want
+#' to REMOVE cells with a.tot < 100, call \code{qc_filter(X, a.tot > 100)}.
+#' In addition, subsets selects a subset of cells to which apply the filter.
+#' i.e. to apply the same area filter but only to cells with f.tot.y values under 50,
+#' call \code{qc_filter(X, a.tot > 100, subset = f.tot.y < 50)}
+#'
+#' It does not delete the filtered rows, it only modifies the QC variable. To effectively
+#' remove them, use \link{qc_execute}.
+#'
+#' @param X cell.data object
+#' @param filter conditions to filter
+#' @param subset subset of cells to select for filtering
 #'
 #' @return cell.data object
 #' @export
@@ -79,14 +87,14 @@ qc_filter <- function(X, filter, subset=NULL){
 
 #*************************************************************************#
 #public
-#removes the last applied QC filter
-#ToDo: allow multiple undos with second argument
-#ToDo: warn for posible outdated variables
+
 #' qc_undo
 #'
-#' @param X
+#' Undoes the last qc_filter applied to the cell.data object.
 #'
-#' @return cell.data object
+#' @param X cell.data
+#'
+#' @return cell.data
 #' @export
 #'
 #' @examples
@@ -122,6 +130,8 @@ qc_undo <- function(X){
 #ToDo: allow subset?
 #' qc_reset
 #'
+#' resets the \code{QC} variable to \code{TRUE}, removing all applied \code{qc_filters}.
+#'
 #' @param X
 #'
 #' @return cell.data object
@@ -149,6 +159,54 @@ qc_reset <- function(X){
     tmp[[hNum]]<-list(type="reset",undo=QC.reseted)
     X$QC.history<-c(X$QC.history,tmp)
     cat("resetting all filters\n")
+
+    return(X)
+}
+
+
+#*************************************************************************#
+
+#ToDo: modify undo value of prevoius QC.history elements
+#ToDo: use subset to code this function
+if(getRversion() >= "2.15.1") utils::globalVariables(c("QC"))
+#' qc_execute
+#'
+#' removes cells with \code{QC == False}.
+#'
+#' @param X cell.data
+#'
+#' @return cell.data
+#' @export
+#'
+#' @examples
+qc_execute <- function(X){
+    QC.attr=attributes(X$data$QC)
+    QC.attr.names=names(QC.attr)
+    QC.history.names=names(X$QC.history)
+    if(is.null(QC.attr.names))
+        hNum="QC0001"
+    else
+        hNum=paste("QC",formatC(1+as.numeric(substring(max(QC.history.names),3,6)),width=4,flag="0"),sep="")
+
+    #calculating the cummulative row exclusion before deleting the registers
+    cer=sum(!X$data$QC)/length(X$data$QC)
+
+    cat("Eliminating ",format(round(100*cer,1),digits=3,nsmall=1),"% of the dataset registers\n",sep="")
+
+    X$data<-subset(X$data,QC)
+    X$data$QC=rep(TRUE,times=dim(X$data)[1])
+
+    tmp<-list()
+    tmp[[hNum]]<-list(type="execute",filter=NA,undo=FALSE,cumulative.exclusion.ratio=cer,subset=NA)
+
+    #setting previous filters as definitive
+    X$QC.history<-
+        lapply(X$QC.history,FUN=function(l){
+            if(is.na(l$undo)) l$undo<-FALSE
+            return(l)
+        })
+
+    X$QC.history<-c(X$QC.history,tmp)
 
     return(X)
 }
