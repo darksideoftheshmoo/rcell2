35e308a1d05eb6417c711de4a2c675a74cae7eca
GasPanic <gaspanic987@gmail.com>
2020-09-29 00:23:00 -0300

    kmeans edits
    Former-commit-id: 2d00b41035e27e8002c728709573c3813fe55dff

--- R/k-means_functions.R
+++ R/k-means_functions.R
@@ -1,20 +1,30 @@
-#' X.normalize
+#' standardize_data
 #'
-#' Normalize X
+#' Standardize data column-wise by calculating the respective Z-scores, i.e., subtracting the mean and dividing by the standard deviation.
 #'
-#' @param X a data.frame with fit.vars
+#' @param x a data.frame with numeric variables suitable for normalization
 #'
-#' @return Normalized X
+#' @return a data.frame with normalized data
 #'
-X.normalize <- function(X){
-  X <- t((t(X)-apply(X, 2, mean))/apply(X, 2, sd))
-  rownames(X) <- NULL
-  colnames(X) <- NULL
-  return(X)
+standardize_data <- function(x){
+  x <- t((t(x)-apply(x, 2, mean))/apply(x, 2, sd))
+  rownames(x) <- NULL
+  colnames(x) <- NULL
+  cbind(rep(1,dim(x)[1]),x) # add intercept column
 }
 
+#' standardise_data
+#'
+#' Standardise data column-wise by calculating the respective Z-scores, i.e., subtracting the mean and dividing by the standard deviation.
+#'
+#' @param x a data.frame with numeric variables suitable for normalization
+#'
+#' @return a data.frame with normalized data
+#'
+standardise_data <- function(x){standardize_data(x)}
 
-#' add_fit_variables
+
+#' calculate_extra_morpho_vars
 #'
 #' Create custom fit variables
 #'
@@ -22,192 +32,199 @@ X.normalize <- function(X){
 #'
 #' @return a cell.data data.frame to which new variables have been added
 #'
-add_fit_variables <- function(x){
-  x <- transform(x, perim.bud=(pi*maj.axis))
-  x <- transform(x, perim.hglass=(pi*min.axis))
-  x <- transform(x, perim.circle=(pi*(min.axis/2)**2))
-  
-  x <- transform(x, a.bud=((pi/4)*(2*min.axis**2-2*min.axis*maj.axis+maj.axis**2)))
-  x <- transform(x, a.hglass=(2*pi*(min.axis/2)**2))
-  x <- transform(x, a.ellipse=(pi*min.axis*maj.axis))
-  x <- transform(x, a.circle=(pi*(min.axis/2)**2))
-  
-  x <- transform(x, elongatedness=(maj.axis**2/min.axis))
-  x <- transform(x, eccentricity=sqrt(1-((min.axis/2)/(maj.axis/2))**2))
-  x <- transform(x, flattening=(maj.axis-min.axis)/(maj.axis+min.axis))
-  x <- transform(x, perim.by.a=(perim/a.tot))
-  x <- transform(x, perim.by.perimellipse=(perim/ellipse.perim))
-  x <- transform(x, perim.by.perimbud=(perim/perim.bud))
-  x <- transform(x, perim.by.spherevol=(perim/sphere.vol))
-  
-  x <- transform(x, a.by.abud=(a.tot/a.bud))
-  x <- transform(x, surf.by.vol=a.surf/sphere.vol)
-  x <- transform(x, surf.by.perim=a.surf/perim)
-  x <- transform(x, fit.var1=fft.stat/elongatedness)
-  x <- transform(x, fit.var2=fft.stat/a.surf)
-  x <- transform(x, fit.var3=fft.stat/a.tot)
-  x <- transform(x, fit.var4=fft.stat/sphere.vol)
-  x <- transform(x, fit.var5=a.surf/elongatedness)
-  x <- transform(x, fit.var6=sphere.vol/elongatedness)
-  x
+calculate_extra_morpho_vars <- function(x){
+  x %>%
+    mutate(perim.bud=(pi*maj.axis),
+           perim.hglass=(pi*min.axis),
+           perim.circle=(pi*(min.axis/2)**2)) %>% 
+    mutate(a.bud=((pi/4)*(2*min.axis**2-2*min.axis*maj.axis+maj.axis**2)),
+           a.hglass=(2*pi*(min.axis/2)**2),
+           a.ellipse=(pi*min.axis*maj.axis),
+           a.circle=(pi*(min.axis/2)**2)) %>% 
+    mutate(elongatedness=(maj.axis**2/min.axis),
+           eccentricity=sqrt(1-((min.axis/2)/(maj.axis/2))**2),
+           flattening=(maj.axis-min.axis)/(maj.axis+min.axis),
+           perim.by.a=(perim/a.tot),
+           perim.by.ellipseperim=(perim/ellipse.perim),
+           perim.by.perimbud=(perim/perim.bud),
+           perim.by.spherevol=(perim/sphere.vol)) %>% 
+    mutate(a.by.abud=(a.tot/a.bud),
+           surf.by.vol=a.surf/sphere.vol,
+           surf.by.perim=a.surf/perim,
+           fit.var1=fft.stat/elongatedness,
+           fit.var2=fft.stat/a.surf,
+           fit.var3=fft.stat/a.tot,
+           fit.var4=fft.stat/sphere.vol,
+           fit.var5=a.surf/elongatedness,
+           fit.var6=sphere.vol/elongatedness)
 }
 
 
-#' get.fit.variables
+#' get_fit_vars
 #'
-#' Extract columns for clustering
+#' Extract specific variables relevant for clustering
 #'
-#' @param x 
-#' @param vars "m" for morpho.vars, "f" for fluor.vars
+#' @param x a cell.data data.frame
+#' @param var_cats a character vector indicating whether morphology ('morpho') and/or fluorescence ('fluor') variables should be used for clustering.
 #'
-#' @return data.frame with fit.variables ("extr.vars") only
+#' @return a reduced cell.data data.frame with selected clustering variables only
 #'
-get.fit.variables <- function(x, vars='m'){
-  f.channels <- substr(unlist(regmatches(names(x), gregexpr(paste0("(f.tot.)+([a-z]{1}$)"), names(x)))), 7, 8)
-  
-  #id.vars <- c('pos', 'cellID', 'time', 'ucid', 't.frame')
+get_fit_vars <- function(x, f.channels, var_cats=NULL, custom_vars=NULL){
+  # Define ID variables; these are used to uniquely identify each row
   id.vars <- c('ucid','t.frame')
   
-  #morpho.vars <- c('xpos', 'ypos', 'fft.stat', 'perim', 'maj.axis', 'min.axis', 'rot.vol', 'con.vol', 'a.surf', 'sphere.vol')
-  morpho.vars <- c('a.tot','fft.stat', 'perim', 'maj.axis', 'min.axis', 'a.surf', 'sphere.vol', 'ellipse.perim')
-  
-  #area.vars <- c('a.tot', 'a.tot.p1', 'a.tot.m1', 'a.tot.m2', 'a.tot.m3')
-  #area.vars <- c('a.tot')
-  
-  #f.vars <- c('f.tot', 'f.tot.p1', 'f.tot.m1', 'f.tot.m2', 'f.tot.m3')
-  f.vars <- c('f.tot')
-  f.vars <- sapply(f.vars, function(x, y) paste(x,y,sep='.'), y=f.channels)
-  f.vars <- c(f.vars, 'f.c', 'f.r', 'f.y')
-  
-  #bg.vars <- c('a.local.bg', 'a.local', 'a.local2.bg', 'a.local2')
-  bg.vars <- c()
-  
-  #bg.f.vars <- c('f.bg', 'f.local.bg', 'f.local2.bg')
-  #bg.f.vars <- c('f.bg', 'f.local.bg')
-  #bg.f.vars <- sapply(bg.f.vars, function(x, y) paste(x,y,sep='.'), y=f.channels)
-  bg.f.vars <- c()
-  
+  ## Collect variables
   default.vars <- names(x)
+  selected.vars <- c(id.vars,custom_vars)
   
-  x <- add_fit_variables(x)
-  
-  new.vars <- names(x)
-  new.vars <- new.vars[!(new.vars%in%default.vars)]
-  
-  all.vars <- names(x)
-  
-  ## Defines whether fluorescence ('f') and/or morphology ('m') variables should be extracted
-  extr.vars <- c()
-  if(sum(grep("m",vars))){
-    morpho.vars <- c(id.vars, morpho.vars, new.vars)
-    extr.vars <- c(extr.vars,morpho.vars)
-  }
-  if(sum(grep("f",vars))){
-    fluor.vars <- c(f.vars, bg.vars, bg.f.vars)
-    extr.vars <- c(extr.vars,fluor.vars)
+  ## Conditionally include morphology variables
+  if("morpho"%in%var_cats){
+    # Set default morphology variables
+    morpho.vars <- c('a.tot','fft.stat', 'perim', 'maj.axis', 'min.axis', 'a.surf', 'sphere.vol', 'ellipse.perim')
+    
+    # Calculate and append custom morphology variables
+    x <- calculate_extra_morpho_vars(x)
+    all.vars <- names(x)
+    extra.morpho.vars <- all.vars[!(all.vars%in%default.vars)]
+    
+    selected.vars <- c(selected.vars,morpho.vars, extra.morpho.vars)
   }
   
-  return(x[,names(x) %in% extr.vars])
+  ## Conditionally include fluorescence variables
+  if("fluor"%in%var_cats){
+    # Select default fluorescence variables
+    fluor.vars <- c("f.tot","f.bg","f.local.bg","f.nucl.tag1","f.nucl.tag3","f.nucl.tag5","f.vacuole","f.bg","f.tot.m1","f.tot.m3")
+    # consider also adding "f"; this requires fixing Rcell2 to automatically calculate "f.t" and "cf.t" variables (if applicable).
+    
+    fluor.vars <- as.vector(sapply(fluor.vars,function(x,y) paste(x,y,sep="."),y=f.channels,simplify=TRUE))
+    fluor.vars <- c(fluor.vars)
+    selected.vars <- c(selected.vars,fluor.vars)
+  }
+  
+  x %>% select(all_of(selected.vars))
+  
+  #return(x[,names(x) %in% selected.vars])
 }
 
-#' kmeans_clustering
+#' K-means clustering 
 #'
-#' Perform k-means clustering on Cell-ID data.
+#' Perform k-means clustering on a Cell-ID data.
 #'
-#' @param x cell.data object
-#' @param k non-negative integer. The desired number of clusters.
-#' @param centroids optional integer vector of length 'k' defining the rows in 'x$data' to be chosen as starting centroids. If not provided, these are selected randomly.
-#' @param max_iter non-negative integer. The maximal number of iterations allowed, if the algorithm has not yet converged.
-#' @param resume logical. If TRUE the algorithm resumes clustering from previously assigned clusters.
-#' @param vars optional character vector
+#' @param x cell.data object or a cell.data data.frame
+#' @param k either a non-negative integer setting the number of clusters, or a data.frame with \code{ucid} and \code{t.frame} pairs giving the rows to be used as starting centroids.
+#' @param max_iter non-negative integer. The maximum number of iterations allowed.
+#' @param resume logical. If \code{TRUE} the algorithm picks up clustering from pre-assigned clusters found in a column with name \code{k} (default), or by the column name passed by the \code{tag_col} argument.
+#' @param tag_col optional string for specifying the name of the column containing pre-defined clusters to be used when \code{resume} is set to \code{TRUE}. Defaults to \code{k}.
+#' @param var_cats optional character vector specifying if morphological (\code{morpho}) and/or fluorescence (\code{fluor}) variables should be used for clustering.
+#' @param custom_vars optional character vector specifying custom variables to be included for clustering.
 #' 
 #' @example 
 #' 
-#' # Define custom classification variables (optional; selected morphology variables used by default)
-#' class.variables = NULL
-#' #class.variables <- c('a.tot','ellipse.perim','perim','maj.axis','min.axis','sphere.vol')
-#' 
-#' # Set number k-mean classes
-#' k_count <- 10
-#' 
-#' # Set max number of iterations (*)
-#' max_iter <- 50 
-#' 
-#' # (*) The function kmeans_clustering runs the algorithm iteratively,
-#' # and keeps track of how many cells changed class between each iteration.
-#' # If for any iteration there were no class updates, the function considers
-#' # the classification complete, and breaks the run. Otherwise, it continues
-#' # the classification until "max.iter" iterations have been completed.
-#' 
-#' # Run k-means classification; returns Cell-ID object with column "k",
-#' # indicating the assigned class for each row/cell
-#' X <- kmeans_clustering(X, k=k_count, max_iter=max_iter, vars=class.variables)
-#'
-#' @return the original cell.data object with appended 'k' and 'k_dist' columns indicating the assigned class and Euclidean distance the assigned centroid, respectively.
+#' @return a cell.data object with columns 'k' and 'k.dist' appended to the data.frame, indicating the assigned class and Euclidean distance to the assigned centroid, respectively.
 #' @export
 #'
-kmeans_clustering <- function(x, k=10, max_iter=100, vars=NULL, resume=FALSE){
-  if(!resume){
-    ## Remove pre-existing 'k' and 'k_dist' columns
-    x$data <- x$data %>% mutate(k = NULL, k_dist = NULL)
+kmeans_clustering <- function(x, k=10, max_iter=100, resume=FALSE, tag_col = 'k', var_cats=c("morpho"),custom_vars=NULL){
+  ## Get filtered data
+  if(is.cell.data(x)){
+    xdata <- x$data[x$data$qc,]
+    f.channels <- x$channels$posfix
+    }
+  else{
+    xdata <- x
+    f.channels <- substr(unlist(regmatches(names(xdata), gregexpr(paste0("(f.tot.)+([a-z]{1}$)"), names(xdata)))), 7, 8)
   }
   
-  ## Get data to classify
-  cdata <-x$data[x$data$qc,]
-  #cellk <- subset(x$data, qc==TRUE)
+  ## Extract columns for computing clustering
+  cdata <- get_fit_vars(xdata,
+                        f.channels,
+                        var_cats,
+                        custom_vars)
   
-  ## Append training variables
-  cdata <- get.fit.variables(cdata)
-  
-  if(!is.null(vars)){
-    vars <- c(c('ucid','t.frame','tag.type'),vars)
-    cdata <- cdata[,names(cdata)%in%vars]
+  ## Remove any rows with NAs
+  na.idx <- unique(unlist(sapply(1:ncol(cdata),function(x,i) which(is.na(x[,i])), x=cdata)))
+  if(length(na.idx)){
+    cdata <- cdata[-na.idx,]
+    xdata <- xdata[-na.idx,]
   }
   
-  ## Remove rows with NAs
-  n.cols <- ncol(cdata)
-  na.idx <- unique(unlist(sapply(1:n.cols,function(x,i) which(is.na(x[,i])), x=cdata)))
-  if(length(na.idx)) cdata <- cdata[-na.idx,]
+  ## Extract id variables (id) and clustering data (cdata)
+  id.vars <- c("ucid","t.frame")
+  fit.vars <- names(cdata)[!(names(cdata)%in%id.vars)]
+  id <- cdata %>% select(any_of(id.vars))
+  cdata <- cdata %>% select(any_of(fit.vars))
   
-  ## Extract ucids (id) and data (Xd)
-  fit.vars <- names(cdata)[!(names(cdata)%in%c('ucid','t.frame','tag.type'))]
-  id <- cdata[,c('ucid','t.frame')]
-  Xd <- cdata[,fit.vars]
+  ## Standardize (normalize) clustering data
+  cdata <- standardize_data(cdata)
   
-  ### RUN k-means classification
-  ## Normalize X
-  Xd <- X.normalize(Xd)
-  
-  ## Randomly pick k samples as initial centroids
-  k.sample <- sample(dim(Xd)[1],k)
-  k.means <- Xd[k.sample,]
-  
-  ## Set random initial centroid assignments
-  k.labs.old <- sample(1:k,length(id),replace=TRUE)
-  
-  ## Loop over centroid calculation as long as labels change
-  ## Limit loop to 100 iterations
-  i.diff <- 1
-  i.count <- 0
-  
-  while(i.diff>0 && i.count<max_iter){
-    ## Calculate distances of each row in Xd to each centroid in k.means
-    dists <- apply(k.means,1,function(x,k) sqrt(rowSums(sweep(x, 2, k)**2)),x=Xd)
+  ### Run k-means classification ####
+  if(resume){
+    ### If clustering takes off from or resumes previous tagging
+    ## Verify that tag_col exists
+    stopifnot("missing k column" = tag_col%in%names(xdata))
     
-    ## Find nearest centroid for each row in Xd
+    ## Extract tag_col
+    tags <- xdata %>% pull(!!tag_col)
+    unique_tags <- sort(unique(tags)[!is.na(unique(tags))])
+    
+    # Ensure numeric, consecutive cluster labels
+    k.labs.current <- match(tags,unique_tags)
+    k.labs.unique <- unique(k.labs.current[!is.na(k.labs.current)])
+    
+    ## Assign random cluster value to any missing values (NA)
+    na.idx <- which(is.na(tags))
+    k.labs.sampled <- sample(k.labs.unique,length(k.labs.current),replace=TRUE)
+    k.labs.current[na.idx] <- k.labs.sampled[na.idx]
+    
+    ## Calculate current cluster centroids
+    k.means <- t(sapply(1:length(unique_tags), function(i,x,n) colMeans(x[which(n==i),]), x=cdata, n=k.labs.current))
+    
+    ## Check for empty clusters, and randomly assign rows
+    missing <- (1:k)[!(1:k%in%unique(k.labs.current))]
+    k.sample <- sample(dim(cdata)[1],length(missing))
+    k.means <- rbind(k.means,cdata[k.sample,])
+  }
+  else{
+    ### Else, if tagging de novo
+    ## Randomly pick k samples as initial cluster centroids
+    k.sample <- sample(dim(cdata)[1],k)
+    k.means <- cdata[k.sample,]
+    
+    ## Set random initial cluster assignments
+    k.labs.current <- sample(1:k,dim(id)[1],replace=TRUE)
+  }
+  
+  ## Loop over centroid calculation as long as labels continue getting updated
+  ## Limit loop to max_iter iterations
+  cat("Running k-means clustering...\n")
+  for(i.count in 1:max_iter){
+    ## Calculate distances of each row in cdata to each centroid in k.means
+    dists <- apply(k.means,1,function(x,k) sqrt(rowSums(sweep(x, 2, k)**2)),x=cdata)
+    
+    ## Find nearest centroid for each row in cdata
     k.labs <- apply(dists,1,function(x) which(x==min(x))[1])
-    i.diff <- sum(k.labs!=k.labs.old)
-    i.count <- i.count + 1
-    message(paste0("Iteration #",i.count,". Updates: ",i.diff))
+    i.diff <- sum(k.labs!=k.labs.current)
           
     ## Calculate new centroids
-    k.means <- t(sapply(1:k, function(i,x,n) colMeans(x[which(n==i),]), x=Xd, n=k.labs))
-    k.labs.old <- k.labs
+    k.means <- t(sapply(1:k, function(i,x,n) colMeans(x[which(n==i),]), x=cdata, n=k.labs))
+    k.labs.current <- k.labs
+    
+    ## Print progress
+    cat("\r  Iteration #",i.count," (",max_iter,")",". Updates: ",i.diff," ",sep="")
+    flush.console() 
+    
+    if(i.diff==0){
+      cat("\nk-means clustering converged successfully.\n")
+      break
+    }
+    i.count <- i.count + 1
+  }
+  if(i.diff>0){
+    cat("\nk-means clustering reached max number of iterations without converging.\nAppending clusters calculated on last iteration.\n")
   }
   
-  ## Sum up data by ucid, centroid, and distance to centroid
+  ## Sum up data by id, cluster, and distance to cluster centroid
   k.iter <- seq_len(k)
-  dists <- apply(k.means,1,function(x,k) sqrt(rowSums(sweep(x, 2, k)**2)),x=Xd)
+  dists <- apply(k.means,1,function(x,k) sqrt(rowSums(sweep(x, 2, k)**2)),x=cdata)
   k.idx <- lapply(k.iter,function(i,l) which(l==i), l=k.labs)
   k.dists <- lapply(k.iter,function(d,k,i) d[k[[i]],i],d=dists,k=k.idx)
   k.ucids <- lapply(k.iter,function(u,k,i) u[k[[i]],], u=id, k=k.idx)
@@ -215,7 +232,11 @@ kmeans_clustering <- function(x, k=10, max_iter=100, vars=NULL, resume=FALSE){
   k.data <- lapply(k.data,function(x) x[order(x[,3]),])
   k.data <- do.call(rbind.data.frame, k.data)
   
-  x$data <- merge(x$data, k.data, all.x=TRUE)
+  ## Remove any pre-existing 'k' and 'k.dist' columns, and append new columns
+  xdata <- xdata %>% mutate(k = NULL, k.dist = NULL) %>% left_join(k.data,by=c("t.frame","ucid"))
   
+  ## Return original object with appended columns
+  if(is.cell.data(x)) x$data <- xdata
+  else x <- xdata
   return(x)
 }
