db34335bfd956e3d6b33edf1e4e753351b4d9e17
Nico <nmendez.ar@gmail.com>
2020-10-15 13:39:58 -0300

    minor improvement
    Former-commit-id: 4dda71d003ce7b0e04e723b054c728ba389b4eee

--- R/magick_helpers.R
+++ R/magick_helpers.R
@@ -36,20 +36,24 @@ magickPaths <- function(cell.data){
 #' Display an image in rmarkdown with knitr
 #'
 #' @param imgs a magick image or cellMagick output
+#' @param .prefix A string prepended to the file name, "" by default.
 #' @param .resize a cellMagick image resize string (default "200x200").
 #' @param .path Directory where the output should be saved.
 #' @return An path to a temporary image file.
 # @examples
 # cell.args <- cellArgs(path = path)
 #' @export
-magickForKnitr <- function(imgs, .resize = "200x200", .path = tempdir()){
+magickForKnitr <- function(imgs, .prefix = "tile", .resize = "200x200", .path = tempdir()){
   
-  temp <- tempfile(tmpdir = .path, fileext = ".png")
+  temp <- tempfile(tmpdir = .path, fileext = ".png", pattern = .prefix)
   
   if(class(imgs) == "list") imgs <- imgs$img
   
   imgs %>% 
     {if(is.null(.resize)) . else magick::image_resize(., .resize)} %>% 
     magick::image_write(path = temp, format = "png")
+  
+  print(temp)
+  
   return(temp)
 }
