7ec806cc9879e36946fad29d0c39a55fca4ae18e
naikymen <you@example.com>
2021-08-02 16:20:50 -0300

    imagej fft macro and better cellid param descriptions
    Former-commit-id: d3877724694735406fcfa226fada6d4b162a5b1d

--- R/imagej_macros.R
+++ R/imagej_macros.R
@@ -5,12 +5,21 @@
 #' @param pic.path Path to the directory containing the image files.
 #' @param imagej.path Path to the ImageJ binary (a path to "ImageJ-linux64" or equivalent).
 #' @param script.path Path to the ImageJ macro. Defaults to built-in macro.
+#' @export
 #' 
 imagej.fft.filter <- function(
   pic.path,
   imagej.path = "~/Software/Fiji.app/ImageJ-linux64",
-  # script.path = "~/Projects/Academia/Doctorado/gitlabs_acl/scripts_confocal_vic/FFT_filter_on_BFs_R.txt"){
   script.path = system.file("inst/FFT_filter_on_BFs_R.txt", package = "rcell2")){
+  
+  is.dir <- file.info(renamed.path)$isdir
+  
+  if(is.dir) {
+    pic.path <- paste0(normalizePath(pic.path), "/")
+  } else {
+    stop("The provided 'pic.path' is not a directory.")
+  }
+  
   system(paste(
     normalizePath(imagej.path),
     "--headless -macro",
