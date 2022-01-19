6812c7bedb84d6e43827a145c9d2ad83e330dd9e
naikymen <you@example.com>
2022-01-19 10:50:25 -0300

    mejoras en imagej fft filter y ejemplos de imagej multipoint tagging
    Former-commit-id: 47f7f9b593578f7eb704ac995fe30460bf98e494

--- R/imagej_macros.R
+++ R/imagej_macros.R
@@ -1,6 +1,8 @@
 #' Run ImageJ FFT filter macro from R
 #' 
-#' Replaces files matching \code{BF.*.tif} in the target directory.
+#' Passes an ImageJ FFT filter on files matching \code{BF.*.tif} in the target directory.
+#' 
+#' The modified images are saved to a new subdirectory with default name: "filtered/".
 #' 
 #' @param pic.path Path to the directory containing the image files.
 #' @param imagej.path Path to the ImageJ binary (a path to "ImageJ-linux64" or equivalent).
@@ -12,7 +14,7 @@ imagej.fft.filter <- function(
   imagej.path = "~/Software/Fiji.app/ImageJ-linux64",
   script.path = system.file("inst/FFT_filter_on_BFs_R.txt", package = "rcell2")){
   
-  is.dir <- file.info(renamed.path)$isdir
+  is.dir <- file.info(pic.path)$isdir
   
   if(is.dir) {
     pic.path <- paste0(normalizePath(pic.path), "/")
